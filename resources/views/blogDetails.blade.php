9cf970ca292b3b8495a2393c3aadec33a25debaf
unknown <islamnasf19@gmail.com>
2025-12-07 14:25:54 +0300

    first commit

--- resources/views/blogDetails.blade.php
+++ resources/views/blogDetails.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ $blog->name ?? 'مقال' }} - Royal View</title>
+    <title>{{ $blog->name ?? 'مقال' }} </title>
     <link rel="icon" type="image/png" href="{{ asset($data->logo ?? 'default-logo.png') }}">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.rtl.min.css">
